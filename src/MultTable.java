f10a2666c0a4ee32cb94a83976a5c680b660a752
YusufAmat <hoccayusuf@gmail.com>
2022-09-19 20:29:56 +0200

    odev3bitti

--- src/MultTable.java
+++ src/MultTable.java
@@ -1,6 +1,11 @@
 public class MultTable {
     public static void main(String[] args) {
         //TODO: Write a Java program that prepares the multiplication tables from 1 to 10.
-
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "x" + j + "=" + i * j);
+            }
+            System.out.println("------------------");
+        }
     }
 }
--- src/TwoNumbers.java
+++ src/TwoNumbers.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class TwoNumbers {
     public static void main(String[] args) {
         //TODO: Write a Java program that will accept two numbers and check if two numbers are equal or not.
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen birinci sayiyi giriniz : ");
+        int sayi1 = scanner.nextInt();
+        System.out.print("Lutfen ikinci sayiyi giriniz : ");
+        int sayi2 = scanner.nextInt();
+        if (sayi1 == sayi2) {
+            System.out.println("Girmis oldugunuz " + sayi1 + " sayisi ile " + sayi2 + " sayisi birbirine esittir.");
+        } else {
+            System.out.println("Girmis oldugunuz " + sayi1 + " sayisi ile " + sayi2 + " sayisi birbirine esit degildir.");
+        }
     }
 }
